cfb5f4b0ae74cf797e514d9bec9170c56693cd6a
Restaurant360 <sistemasdigitales360@gmail.com>
2025-07-25 04:55:21 -0500

    Fix useEffect import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth } from './hooks/useAuth'
 import { usePedidos } from './hooks/usePedidos'
 import { useEstadisticas } from './hooks/useEstadisticas'
